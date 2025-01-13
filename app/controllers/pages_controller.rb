b4a3715cbc839c03578807cd2990358137f1b550
maxime nogues <maxime.nogues1408@gmail.com>
2025-01-13 15:57:57 +0100

    Done, still have to do some styling

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @response = params[:question]
+    @coach_answer = ""
+    
+    if @response == "I am going to work"
+      @coach_answer = "Great !"
+    elsif @response.end_with?("?")
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
